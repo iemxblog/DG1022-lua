facca7596e7bd8512dbc4a8bce3e3b62bfb37599
iemxblog <iemxblog@gmail.com>
2018-05-11 12:15:55 +0200

    correction3 on 11/05/2018

--- correction3.lua
+++ correction3.lua
@@ -1,12 +1,12 @@
 DG1022 = require("DG1022")
 lib = require("lib")
 
-local fs = {77, 276, 533, 934, 1532, 2126, 2565, 3003}
+local fs = {4, 11, 20, 36, 52, 72, 96, 130, 21, 38, 68, 94, 151, 204} 
 
 gen = DG1022:new()
 gen:sinusoid()
 gen:offset(2.5)
-gen:voltage(4.990)
+gen:voltage(5.0)
 gen:phase(89)
 
 for _, f in ipairs(fs) do
