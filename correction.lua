d1d5aee851f1efd5c6e1c2af9e348c7f0cf5efc4
iemxblog <iemxblog@gmail.com>
2018-07-10 16:53:27 +0200

    correction.lua on 10/07/2017

--- correction.lua
+++ correction.lua
@@ -1,28 +1,29 @@
 DG1022 = require("DG1022")
 lib = require("lib")
 
-fs = 0.000441 -- start
-fe = 0.000520 -- end
+fs = 0.002989 -- start
+fe = 0.003004 -- end
+t = 2         -- time for each frequency
+n = 8         -- number of repetitions
 
 gen = DG1022:new()
 gen:sinusoid()
 gen:offset(2.5)
-gen:voltage(4.990)
-gen:phase(30)
+gen:voltage(5.0)
+gen:phase(89)
 gen:frequency(fs)
 gen:output(true)
 
 for i = 0, fe-fs, 0.000002 do
-    for j = 1, 9 do
+    for j = 1, n do
         local f1 = fs+i
         local f2 = fs+i+0.000001
         print(f1)
         gen:frequency(f1)
-        lib.sleep(1.1)
+        lib.sleep(t)
         print(f2)
         gen:frequency(f2)
-        lib.sleep(1.1)
-        --print(f1, f2)
+        lib.sleep(t)
     end
 end 
 
