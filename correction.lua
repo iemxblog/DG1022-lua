ae34c3c2a1ea09494821e7b9b9d386a81a580e8e
iemxblog <iemxblog@gmail.com>
2018-07-10 17:16:56 +0200

    correction.lua on 10/07/2018 (2nd time)

--- correction.lua
+++ correction.lua
@@ -1,8 +1,8 @@
 DG1022 = require("DG1022")
 lib = require("lib")
 
-fs = 0.002989 -- start
-fe = 0.003004 -- end
+fs = 0.003083 -- start
+fe = 0.003098 -- end
 t = 2         -- time for each frequency
 n = 8         -- number of repetitions
 
